c31c6e0e2d42a93dc95a010fdefa9bd9bec8cab1
shaun.agostinho <shaunagostinho@gmail.com>
2022-08-29 16:11:58 -0700

    added network info

--- main.go
+++ main.go
@@ -15,16 +15,6 @@ var (
 	Debug = false
 )
 
-/*
-TODO
-
-api to:
-get configuration as agent
-post icmp & mtr check as agent
-
-
-*/
-
 func main() {
 	var err error
 	if err != nil {
@@ -59,14 +49,59 @@ func main() {
 		respB := agent_models.ApiConfigResponse{}
 		respB.Response = 200
 
+		var data []*agent_models.MtrTarget
+		err = json.Unmarshal(c.Body(), &data)
+		if err != nil {
+			log.Errorf("2 %s", err)
+			respB.Response = 500
+		}
+		jRespB, err := json.Marshal(respB)
+		if err != nil {
+			log.Errorf("3 Unable to marshal API response.")
+		} else {
+			log.Warnf("%s", string(jRespB))
+			return c.SendString(string(jRespB)) // => ✋ good
+		}
+
+		return c.SendString("Something went wrong...") // => ✋
+	})
+
+	app.Post("/v1/agent/update/network", func(c *fiber.Ctx) error {
+		c.Accepts("application/json") // "application/json"
+		respB := agent_models.ApiConfigResponse{}
+		respB.Response = 200
+
 		log.Warnf("%s", c.Body())
 
-		var icmpData []*agent_models.MtrTarget
+		var data *agent_models.NetworkInfo
+		err = json.Unmarshal(c.Body(), &data)
+		if err != nil {
+			log.Errorf("2 %s", err)
+			respB.Response = 500
+		}
+		jRespB, err := json.Marshal(respB)
+		if err != nil {
+			log.Errorf("3 Unable to marshal API response.")
+		} else {
+			log.Warnf("%s", string(jRespB))
+			return c.SendString(string(jRespB)) // => ✋ good
+		}
+
+		return c.SendString("Something went wrong...") // => ✋
+	})
+	app.Post("/v1/agent/update/speedtest", func(c *fiber.Ctx) error {
+		c.Accepts("application/json") // "application/json"
+		respB := agent_models.ApiConfigResponse{}
+		respB.Response = 200
+
+		var data agent_models.SpeedTestInfo
+
+		//log.Warnf("%s", c.Body())
 
 		//fmt.Println(res["json"])
 
 		//log.Infof("%s", string(jMar))
-		err = json.Unmarshal(c.Body(), &icmpData)
+		err = json.Unmarshal(c.Body(), &data)
 		if err != nil {
 			log.Errorf("2 %s", err)
 			respB.Response = 500
@@ -88,12 +123,8 @@ func main() {
 		respB := agent_models.ApiConfigResponse{}
 		respB.Response = 200
 
-		var icmpData []*agent_models.IcmpTarget
-
-		//fmt.Println(res["json"])
-
-		//log.Infof("%s", string(jMar))
-		err = json.Unmarshal(c.Body(), &icmpData)
+		var data []*agent_models.IcmpTarget
+		err = json.Unmarshal(c.Body(), &data)
 		if err != nil {
 			log.Errorf("2 %s", err)
 			respB.Response = 500

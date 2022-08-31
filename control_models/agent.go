0b59463c0348c4a8810ce69cae117dbb90b42a3e
Shaun Agostinho <shaunagostinho@gmail.com>
2022-08-31 07:37:05 -0700

    agent config

--- control_models/agent.go
+++ control_models/agent.go
@@ -4,7 +4,7 @@ import "github.com/sagostin/netwatcher-agent/agent_models"
 
 type Agent struct {
 	Site        string                   `json:"site"` // _id of mongo object
-	CheckConfig agent_models.CheckConfig `json:"check_config"`
+	AgentConfig agent_models.AgentConfig `json:"agent_config"`
 	Pin         string                   `json:"pin"` // used for registration & authentication
 	Hash        string                   `json:"hash"`
 	// pin can be regenerated, by setting hash blank, and when registering agents, it checks for blank hashs.

435c4e3ba11d55499e488496c31108fc0db34c08
Shaun Agostinho <shaunagostinho@gmail.com>
2022-08-31 22:30:15 -0700

    login and auth sessions

--- session.go
+++ session.go
@@ -1,27 +1,47 @@
 package main
 
 import (
+	"errors"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/session"
 	log "github.com/sirupsen/logrus"
 )
 
-func CreateSession(c *fiber.Ctx, sessions session.Store) (bool, error) {
+func createSession(c *fiber.Ctx, sessions session.Store) error {
 	store, err := sessions.Get(c) // get/create new session
 	if err != nil {
 		log.Errorf("%s", err)
 	}
 	defer store.Save()
-	return true, nil
+	return nil
 }
 
-func ValidateSession(c *fiber.Ctx) (bool, error) {
-	s := session.New()
-	store, err := s.Get(c) // get/create new session
+func validateSession(c *fiber.Ctx, sessions session.Store) (bool, error) {
+	store, err := sessions.Get(c) // get/create new session
 	if err != nil {
 		log.Errorf("%s", err)
 		return false, err
 	}
+
+	if store.Get("email") == nil {
+		return true, errors.New("no email, not logged in")
+	}
+
+	defer store.Save()
+	return true, nil
+}
+
+func loginSession(c *fiber.Ctx, sessions session.Store) (bool, error) {
+	store, err := sessions.Get(c) // get/create new session
+	if err != nil {
+		log.Errorf("%s", err)
+		return false, err
+	}
+
+	if store.Get("email") == nil {
+		return true, errors.New("no email, not logged in")
+	}
+
 	defer store.Save()
 	return true, nil
 }

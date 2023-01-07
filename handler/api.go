d7befac13229a00bbd47fb70d3c0f728bb194858
Shaun Agostinho <shaunagostinho@gmail.com>
2023-01-07 10:55:14 -0800

    api fix

--- handler/api.go
+++ handler/api.go
@@ -1,19 +1,18 @@
 package handler
 
 import (
-	"github.com/netwatcherio/netwatcher-agent/checks"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type Data struct {
-	PIN    string             `json:"pin,omitempty"`
-	ID     string             `json:"id,omitempty"`
-	Checks []checks.CheckData `json:"checks,omitempty"`
-	Error  string             `json:"error,omitempty"`
+type ApiRequest struct {
+	PIN   string      `json:"pin,omitempty"`
+	ID    string      `json:"id,omitempty"`
+	Data  interface{} `json:"data,omitempty"`
+	Error string      `json:"error,omitempty"`
 }
 
-func (d *Data) GenerateCheckData(db *mongo.Database) error {
+func (d *ApiRequest) GenerateCheckData(db *mongo.Database) error {
 	hexId, err := primitive.ObjectIDFromHex(d.ID)
 	if err != nil {
 		return err

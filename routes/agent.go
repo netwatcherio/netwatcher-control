a9364b30cd7707800acab2d9a7a1cfc9006336dd
Shaun Agostinho <shaunagostinho@gmail.com>
2023-01-18 14:49:36 -0800

    added ability to get all agents for a site

--- routes/agent.go
+++ routes/agent.go
@@ -18,21 +18,50 @@ func (r *Router) getAgent() {
 		t := c.Locals("user").(*jwt.Token)
 		_, err := auth.GetUser(t, r.DB)
 		if err != nil {
-			return c.JSON(err)
+			return c.SendStatus(fiber.StatusInternalServerError)
 		}
 
 		aId, err := primitive.ObjectIDFromHex(c.Params("agent"))
 		if err != nil {
-			return c.JSON(err)
+			return c.SendStatus(fiber.StatusInternalServerError)
 		}
 
 		a := agent.Agent{ID: aId}
 		err = a.Get(r.DB)
 		if err != nil {
-			return c.JSON(err)
+			return c.SendStatus(fiber.StatusInternalServerError)
 		}
 
 		return c.JSON(a)
+	})
+}
+
+func (r *Router) getAgents() {
+	r.App.Get("/agents/:siteid?", func(c *fiber.Ctx) error {
+		c.Accepts("application/json") // "Application/json"
+		t := c.Locals("user").(*jwt.Token)
+		_, err := auth.GetUser(t, r.DB)
+		if err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		aId, err := primitive.ObjectIDFromHex(c.Params("siteid"))
+		if err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		a := site.Site{ID: aId}
+		err = a.Get(r.DB)
+		if err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		agents, err := a.GetAgents(r.DB)
+		if err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		return c.JSON(agents)
 	})
 }
 
@@ -71,23 +100,23 @@ func (r *Router) agentNew() {
 		t := c.Locals("user").(*jwt.Token)
 		_, err := auth.GetUser(t, r.DB)
 		if err != nil {
-			return c.JSON(err)
+			return c.SendStatus(fiber.StatusInternalServerError)
 		}
 
 		sId, err := primitive.ObjectIDFromHex(c.Params("siteid"))
 		if err != nil {
-			return c.JSON(err)
+			return c.SendStatus(fiber.StatusInternalServerError)
 		}
 
 		s := site.Site{ID: sId}
 		err = s.Get(r.DB)
 		if err != nil {
-			return c.JSON(err)
+			return c.SendStatus(fiber.StatusInternalServerError)
 		}
 
 		cAgent := new(agent.Agent)
 		if err := c.BodyParser(cAgent); err != nil {
-			return c.JSON(err)
+			return c.SendStatus(fiber.StatusInternalServerError)
 		}
 
 		cAgent.Site = s.ID
@@ -95,7 +124,7 @@ func (r *Router) agentNew() {
 		err = cAgent.Create(r.DB)
 		if err != nil {
 			log.Error(err)
-			return c.JSON(err)
+			return c.SendStatus(fiber.StatusInternalServerError)
 		}
 
 		return c.SendStatus(fiber.StatusOK)
